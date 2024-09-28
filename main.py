d04319def6794ebdc9330651cf76069a7ca26fff
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-09-28 15:59:52 +0530

    add project title

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -10,3 +11,18 @@ with col1:
 with col2:
     st.title("Arman Ul Haq")
     st.info("I am a passionate Python developer eager to learn and create innovative projects. With a deep interest in problem-solving and continuous learning, I enjoy exploring new technologies and applying them to real-world challenges. My journey involves pushing boundaries to enhance my skills and develop impactful solutions.")
+
+content2 = "Below you can find some of the apps I have built in Python. Feel free to contact me!"
+st.write(content2)
+
+
+col3, col4 = st.columns(2)
+df=pd.read_csv('data.csv', sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
